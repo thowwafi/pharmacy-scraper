890bcea22225d6d125ba2bcc7bd782035ea8a88a
Thowwafi <thowwafi@gmail.com>
2021-06-19 21:35:45 +0700

    update scraper to get subpages and contents

--- scraper.py
+++ scraper.py
@@ -10,31 +10,19 @@ from fuzzywuzzy import process
 from fuzzywuzzy import fuzz
 import os
 from slugify import slugify
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 import requests
-from urllib3 import request
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from Url import Url
 
 
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 HOME = os.getcwd()
+DATA_PATH = "./source/client_list.csv"
 
 
 def closeMatches(word, patterns):
     print(get_close_matches(word, patterns))
-
-
-def get_subpages(driver, homepage_link):
-    driver.get(homepage_link)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    links = []
-    for link in soup.find_all('a', href=True):
-        if 'https' not in link.get('href'):
-            a_href = homepage_link + link.get('href')
-        else:
-            a_href = link.get('href')
-        print("subpage", a_href)
-        links.append(a_href)
-    return links
 
 
 def validate_homepage(link, row):
@@ -83,10 +71,8 @@ def google_search(row, total, additional_params):
         # domain = urlparse(homepage_link).netloc
         # if not domain.startswith("http"):
         #     domain = f"{urlparse(homepage_link).scheme}://{domain}"
-        # import pdb; pdb.set_trace()
 
         suggestions.append(homepage_link)
-    # import pdb; pdb.set_trace()
     driver.quit()
     return web_button_link, suggestions
 
@@ -119,14 +105,36 @@ def get_validated_urls(web_button_link, suggestions):
     return validated_urls
 
 
-def check_validated_urls(folder_path, file_path):
+def check_validated_urls(file_path):
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding='utf-8') as fn:
             data = json.load(fn)
         return bool(data.get('suggestions'))
+    return False
 
 
-DATA_PATH = "./source/client_list.csv"
+def get_subpages(url):
+    response = requests.get(url, verify=False)
+    print("url", url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        links = []
+        for link in soup.find_all('a', href=True):
+            the_href = link.get('href')
+            if 'http' not in the_href and the_href.startswith("/"):
+                print("link.get('href')", link.get('href'))
+                # if link.get('href').startswith("/") and url.endswith("/"):
+                #     new_url = url[1:]
+                # else:
+                #     new_url = url
+                a_href = urljoin(url, link.get('href'))
+            else:
+                continue
+            print("subpage", a_href)
+            links.append(a_href)
+    return links
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--search-qty', required=True)
@@ -146,30 +154,59 @@ if __name__ == '__main__':
     print('continue_scraper', continue_scraper)
     df = read_data(DATA_PATH)
     if end_index == 0:
-        end_index = 20
+        end_index = len(df)
     for index, row in df.iterrows():
         if index >= start_index and  index <= end_index:
             slug_name = f"{slugify(row['name'])}-{slugify(row['city'])}"
             folder_path = os.path.join(HOME, "data", slug_name)
-            file_path = os.path.join(folder_path, slug_name+".json")
-            if continue_scraper:
-                existed = check_validated_urls(folder_path, file_path)
-                if existed:
-                    continue
+            overview_path = os.path.join(folder_path, 'overview')
+            if not os.path.exists(overview_path):
+                os.makedirs(overview_path)
+            file_path = os.path.join(overview_path, slug_name+".json")
+            if continue_scraper and check_validated_urls(file_path):
+                continue
             additional_params = ''
             web_button_link, suggestions = google_search(row, search_qty, additional_params)
             validated_urls = get_validated_urls(web_button_link, suggestions)
-            if not validated_urls:
-                additional_params = 'address'
-                web_button_link, suggestions = google_search(row, search_qty, additional_params)
-                validated_urls = get_validated_urls(web_button_link, suggestions)
+            # if not validated_urls:
+            #     additional_params = 'address'
+            #     web_button_link, suggestions = google_search(row, search_qty, additional_params)
+            #     validated_urls = get_validated_urls(web_button_link, suggestions)
 
             str_row = row.to_json()
             row_json = ast.literal_eval(str_row)
+
+            for valid_url in validated_urls:
+                valid_url['subpages'] = get_subpages(valid_url.get('url'))
+            
             row_json['suggestions'] = validated_urls
             row_json['_id'] = index
-
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
             with open(file_path, 'w', encoding='utf-8') as fn:
                 json.dump(row_json, fn, indent=4, ensure_ascii=False)
+
+            for sub_urls in row_json['suggestions']:
+            
+                for sub in sub_urls.get('subpages'):
+                    print('sub', sub)
+                    try:
+                        resp = requests.get(sub, verify=False)
+                    except Exception as e:
+                        print('error', e)
+                        continue
+                    soup = BeautifulSoup(resp.content, 'html.parser')
+                    thisdata = {
+                        "url": sub,
+                        "text": soup.text.rstrip().replace("\n", "")
+                    }
+                    parsed = urlparse(sub)
+                    slug_domain = parsed.netloc
+                    subpath = os.path.join(folder_path, 'subpages', slug_domain)
+                    if not os.path.exists(subpath):
+                        os.makedirs(subpath)
+                    filename = f"{parsed.path}-{parsed.params}-{parsed.query}-{parsed.fragment}"
+                    slug_filename = slugify(filename)
+                    if not slug_filename:
+                        slug_filename = 'homepage'
+                    filepath = os.path.join(subpath, slug_filename + ".json")
+                    with open(filepath, 'w', encoding='utf-8') as fn:
+                        json.dump(thisdata, fn, indent=4, ensure_ascii=False)
