c6dce6219a96502ae9c51ec3db4c9a19e3a65c81
Thowwafi <thowwafi@gmail.com>
2021-06-19 09:14:39 +0700

    major update

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from difflib import get_close_matches
 from bs4 import BeautifulSoup
 import pandas as pd
 from selenium import webdriver
@@ -6,12 +7,20 @@ import json
 import ast
 import argparse
 from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
 import os
 from slugify import slugify
 from urllib.parse import urlparse
+import requests
+from urllib3 import request
+from Url import Url
 
 
-home = os.getcwd()
+HOME = os.getcwd()
+
+
+def closeMatches(word, patterns):
+    print(get_close_matches(word, patterns))
 
 
 def get_subpages(driver, homepage_link):
@@ -28,108 +37,139 @@ def get_subpages(driver, homepage_link):
     return links
 
 
-def validate_homepage(driver, link, row):
-    try:
-        driver.get(link)
-    except InvalidArgumentException as e:
-        print(e)
-        print(link)
-        return False
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    datas = [row['name'], row['street'], row['zip'], row['city']]
+def validate_homepage(link, row):
+    session = requests.Session()
+    session.trust_env = False
+    response = session.get(link, verify=False)
+    web_status = response.status_code
     got_it = []
-    if driver.title == 'Privacy error':
-        got_it.append(False)
-    for data in datas:
-        res = soup(text=lambda t: data in t)
-        if res:
-            got_it.append(True)
-        else:
-            got_it.append(False)
+    if web_status == 200:
+        web_content = response.content
+        soup = BeautifulSoup(web_content, 'html.parser')
+        words = [row['name'], row['street'], row['zip'], row['city']]
+        for word in words:
+            res = soup(text=lambda t: word in t)
+            # matches = closeMatches(word, soup.text.split(' '))
+            if res:
+                print('data', word)
+                got_it.append(True)
+            else:
+                got_it.append(False)
     return True in got_it
 
 
-def google_search(row, total):
+def google_search(row, total, additional_params):
+    driver = webdriver.Chrome(executable_path="./chromedriver")
     name = row['name']
     street = row['street']
     zip_code = row['zip']
     city = row['city']
-    query = name.replace("&", "and").replace(' ', '+')
-    URL = f"https://google.com/search?q={query}+{street}+{zip_code}+{city}"
+    if additional_params:
+        query = f"{name}+{street}+{zip_code}+{city}+{additional_params}"
+    else:
+        query = f"{name}+{street}+{zip_code}+{city}"
+    query = query.replace("&", "and").replace(' ', '+')
+    URL = f"https://google.com/search?q={query}"
     print("name", f"{name} {city}")
-    driver = webdriver.Chrome(executable_path="./chromedriver")
     driver.get(URL)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    homepages = []
+    suggestions = []
     web_button = soup.find('a', class_='ab_button')
-    web_button_link = ""
+    web_button_link = None
     if web_button:
         web_button_link = web_button.get('href')
-        # homepages.append(homepage_link)
     for link in soup.findAll("h3")[:int(total)]:
         homepage_link = link.find_parent('a').get('href')
-        # is_valid = validate_homepage(driver, homepage_link, row)
-        # links = get_subpages(driver, homepage_link)
-        # data = {
-        #     "homepage": homepage_link,
-        #     "subpages": links
-        # }
-        domain = urlparse(homepage_link).netloc
-        if not domain.startswith("http"):
-            domain = f"{urlparse(homepage_link).scheme}://{domain}"
+        # domain = urlparse(homepage_link).netloc
+        # if not domain.startswith("http"):
+        #     domain = f"{urlparse(homepage_link).scheme}://{domain}"
+        # import pdb; pdb.set_trace()
 
-        homepages.append(domain)
-    results = process.extract(name+city, homepages, limit=int(total)+1)
-    urls = []
+        suggestions.append(homepage_link)
+    # import pdb; pdb.set_trace()
+    driver.quit()
+    return web_button_link, suggestions
+
+
+def read_data(path):
+    return pd.read_csv(path, sep=";", encoding='cp1252')
+
+
+def get_validated_urls(web_button_link, suggestions):
+    validated_urls = []
     if web_button_link:
-        is_valid = validate_homepage(driver, web_button_link, row)
+        is_valid = validate_homepage(web_button_link, row)
         if is_valid:
             print("valid", web_button_link)
-            urls.append(web_button_link)
-    for res in results:
-        if res[1] > 50:
-            is_valid = validate_homepage(driver, res[0], row)
+            validated_urls.append({
+                "url": web_button_link,
+                "score": 100
+            })
+    for suggestion_link in suggestions:
+        ratio = fuzz.ratio(row['name']+row['city'], suggestion_link)
+        url_not_scraped = all(url['url'] != suggestion_link for url in validated_urls)
+        if ratio > 50 and url_not_scraped:
+            is_valid = validate_homepage(suggestion_link, row)
             if is_valid:
-                print("valid", res[0])
-                urls.append(res[0])
-            else:
-                print("invalid", res[0])
-    driver.quit()
-    return urls
+                print("valid", suggestion_link)
+                validated_urls.append({
+                    "url": suggestion_link,
+                    "score": ratio
+                })
+    return validated_urls
 
 
-def run_scraper(total):
-    df = pd.read_csv("./data/client_list.csv", sep=";", encoding='cp1252')
-    all_data = []
-    for index, row in df.iterrows():
-        if index > 29 and index < 100:
-            links = google_search(row, total)
-            str_row = row.to_json()
-            row_json = ast.literal_eval(str_row)
-            row_json['suggestions'] = list(set(links))
-            row_json['_id'] = index
-            slug_name = f"{slugify(row['name'])}-{slugify(row['city'])}"
-            folder_path = os.path.join(home, "data", slug_name)
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
-            file_path = os.path.join(folder_path, slug_name+".json")
-            with open(file_path, 'w', encoding='utf-8') as fn:
-                json.dump(row_json, fn, indent=4, ensure_ascii=False)
-            # str_row = row.to_json()
-            # row_json = ast.literal_eval(str_row)
-            # row_json['suggestions'] = links
-            # row_json['_id'] = index
-            # all_data.append(row_json)
-    
-    # with open("all_data.json", 'w', encoding='utf-8') as fn:
-    #     json.dump(all_data, fn, indent=4, ensure_ascii=False)
+def check_validated_urls(folder_path, file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as fn:
+            data = json.load(fn)
+        return bool(data.get('suggestions'))
 
 
+DATA_PATH = "./source/client_list.csv"
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-q', required=True)
+    parser.add_argument('--search-qty', required=True)
+    # parser.add_argument('--start-over', nargs='?', const=1, type=bool)
+    parser.add_argument('--start-index', nargs='?', const=1, type=int, default=0)
+    parser.add_argument('--end-index', nargs='?', const=1, type=int, default=0)
+    parser.add_argument('--continue-scraper', nargs='?', const=True, type=bool, default=False)
     args = parser.parse_args()
 
-    total_suggestion = args.q
+    search_qty = args.search_qty
+    start_index = args.start_index
+    end_index = args.end_index
+    continue_scraper = args.continue_scraper
+    print("search_qty", search_qty)
+    print("start_index", start_index)
+    print("end_index", end_index)
+    print('continue_scraper', continue_scraper)
+    df = read_data(DATA_PATH)
+    if end_index == 0:
+        end_index = 20
+    for index, row in df.iterrows():
+        if index >= start_index and  index <= end_index:
+            slug_name = f"{slugify(row['name'])}-{slugify(row['city'])}"
+            folder_path = os.path.join(HOME, "data", slug_name)
+            file_path = os.path.join(folder_path, slug_name+".json")
+            if continue_scraper:
+                existed = check_validated_urls(folder_path, file_path)
+                if existed:
+                    continue
+            additional_params = ''
+            web_button_link, suggestions = google_search(row, search_qty, additional_params)
+            validated_urls = get_validated_urls(web_button_link, suggestions)
+            if not validated_urls:
+                additional_params = 'address'
+                web_button_link, suggestions = google_search(row, search_qty, additional_params)
+                validated_urls = get_validated_urls(web_button_link, suggestions)
 
-    run_scraper(total_suggestion)
+            str_row = row.to_json()
+            row_json = ast.literal_eval(str_row)
+            row_json['suggestions'] = validated_urls
+            row_json['_id'] = index
+
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            with open(file_path, 'w', encoding='utf-8') as fn:
+                json.dump(row_json, fn, indent=4, ensure_ascii=False)
